18d2a2b926d412cb62a6525b7dc82dc4c324e170
ravimediacorp <ravikant.gandhi@mediacorp.com.sg>
2018-08-29 15:08:04 +0800

    Initial commit

---
+++ capture.js
@@ -0,0 +1,31 @@
+// capture.js
+
+const puppeteer = require('puppeteer')
+const devices = require('puppeteer/DeviceDescriptors')
+
+const captureScreenshots = async () => {
+  const devicesToEmulate = [
+    'iPhone 6',
+    'iPhone 6 landscape',
+    'iPhone 6 Plus',
+    'Nexus 5',
+    'Nexus 6',
+    'iPad Pro'
+  ]
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  // capture a screenshot of each device we wish to emulate (`devicesToEmulate`)
+  for (let device of devicesToEmulate) {
+    await page.emulate(devices[device])
+    await page.goto('https://www.google.com/')
+    let img = await page.screenshot({path: `${device}.png`, fullPage: false})
+    console.log(`${device} done`);
+    break;
+  }
+
+  await browser.close()
+}
+
+captureScreenshots()
